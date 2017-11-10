55624f59074fd63bd94410a56f47c733485874fd
C?dric Krier <ced@b2ck.com>
2017-11-10 17:21:07 +0100

    Build quietly

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build --rm --no-cache -t tryton/tryton:4.6 -t tryton/tryton:latest 4.6
-docker build --rm --no-cache -t tryton/tryton:4.4 4.4
+docker build -q --rm --no-cache -t tryton/tryton:4.6 -t tryton/tryton:latest 4.6
+docker build -q --rm --no-cache -t tryton/tryton:4.4 4.4
 
 docker push tryton/tryton:4.4
 docker push tryton/tryton:4.6
