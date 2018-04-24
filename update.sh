53df288277bbc68579994bec929c2f446a069905
C?dric Krier <ced@b2ck.com>
2018-04-24 10:57:20 +0200

    Add 4.8 series
    issue7387
    review38191002

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-docker build -q --rm --no-cache -t tryton/tryton:4.6 -t tryton/tryton:latest 4.6
-docker build -q --rm --no-cache -t tryton/tryton:4.6-office -t tryton/tryton:office 4.6/office
+docker build -q --rm --no-cache -t tryton/tryton:4.8 -t tryton/tryton:latest 4.8
+docker build -q --rm --no-cache -t tryton/tryton:4.8-office -t tryton/tryton:office 4.8/office
+docker build -q --rm --no-cache -t tryton/tryton:4.6 4.6
+docker build -q --rm --no-cache -t tryton/tryton:4.6-office 4.6/office
 docker build -q --rm --no-cache -t tryton/tryton:4.4 4.4
 docker build -q --rm --no-cache -t tryton/tryton:4.4-office 4.4/office
 
@@ -9,5 +11,7 @@ docker push tryton/tryton:4.4-office
 docker push tryton/tryton:4.4
 docker push tryton/tryton:4.6-office
 docker push tryton/tryton:4.6
+docker push tryton/tryton:4.8-office
+docker push tryton/tryton:4.8
 docker push tryton/tryton:office
 docker push tryton/tryton:latest
