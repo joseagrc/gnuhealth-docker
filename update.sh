f8eaf87372a5a1f5955ece502972891d3709476a
C?dric Krier <ced@b2ck.com>
2019-05-06 17:32:00 +0200

    Add 5.2 series

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-docker build -q --rm --no-cache -t tryton/tryton:5.0 -t tryton/tryton:latest 5.0
-docker build -q --rm --no-cache -t tryton/tryton:5.0-office -t tryton/tryton:office 5.0/office
+docker build -q --rm --no-cache -t tryton/tryton:5.2 -t tryton/tryton:latest 5.2
+docker build -q --rm --no-cache -t tryton/tryton:5.2-office -t tryton/tryton:office 5.2/office
+docker build -q --rm --no-cache -t tryton/tryton:5.0 5.0
+docker build -q --rm --no-cache -t tryton/tryton:5.0-office 5.0/office
 docker build -q --rm --no-cache -t tryton/tryton:4.8 4.8
 docker build -q --rm --no-cache -t tryton/tryton:4.8-office 4.8/office
 docker build -q --rm --no-cache -t tryton/tryton:4.6 4.6
@@ -17,5 +19,7 @@ docker push tryton/tryton:4.8-office
 docker push tryton/tryton:4.8
 docker push tryton/tryton:5.0-office
 docker push tryton/tryton:5.0
+docker push tryton/tryton:5.2-office
+docker push tryton/tryton:5.2
 docker push tryton/tryton:office
 docker push tryton/tryton:latest
