0bd5b328a6b01123f6561d7690a8d74f33a65cad
C?dric Krier <ced@b2ck.com>
2019-11-23 02:02:12 +0100

    Stop building 4.4, 4.6 and 4.8 series

--- update.sh
+++ update.sh
@@ -6,19 +6,7 @@ docker build -q --rm --no-cache -t tryton/tryton:5.2  5.2
 docker build -q --rm --no-cache -t tryton/tryton:5.2-office 5.2/office
 docker build -q --rm --no-cache -t tryton/tryton:5.0 5.0
 docker build -q --rm --no-cache -t tryton/tryton:5.0-office 5.0/office
-docker build -q --rm --no-cache -t tryton/tryton:4.8 4.8
-docker build -q --rm --no-cache -t tryton/tryton:4.8-office 4.8/office
-docker build -q --rm --no-cache -t tryton/tryton:4.6 4.6
-docker build -q --rm --no-cache -t tryton/tryton:4.6-office 4.6/office
-docker build -q --rm --no-cache -t tryton/tryton:4.4 4.4
-docker build -q --rm --no-cache -t tryton/tryton:4.4-office 4.4/office
 
-docker push tryton/tryton:4.4-office
-docker push tryton/tryton:4.4
-docker push tryton/tryton:4.6-office
-docker push tryton/tryton:4.6
-docker push tryton/tryton:4.8-office
-docker push tryton/tryton:4.8
 docker push tryton/tryton:5.0-office
 docker push tryton/tryton:5.0
 docker push tryton/tryton:5.2-office
