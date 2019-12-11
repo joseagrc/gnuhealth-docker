7a11657e8003dc1aad761587dca6103f1b0f4ed1
C?dric Krier <ced@b2ck.com>
2019-12-11 22:20:57 +0100

    Run tests before pushing images
    issue8283
    review262561002

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 docker build -q --rm --no-cache -t tryton/tryton:5.4 -t tryton/tryton:latest 5.4
 docker build -q --rm --no-cache -t tryton/tryton:5.4-office -t tryton/tryton:office 5.4/office
@@ -6,6 +7,10 @@ docker build -q --rm --no-cache -t tryton/tryton:5.2  5.2
 docker build -q --rm --no-cache -t tryton/tryton:5.2-office 5.2/office
 docker build -q --rm --no-cache -t tryton/tryton:5.0 5.0
 docker build -q --rm --no-cache -t tryton/tryton:5.0-office 5.0/office
+
+docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.4 python3 -m trytond.tests.run-tests -m
+docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.2 python3 -m trytond.tests.run-tests -m
+docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.0 python3 -m trytond.tests.run-tests -m
 
 docker push tryton/tryton:5.0-office
 docker push tryton/tryton:5.0
