b9ece790ab60b401c1cf17fe0046c699f903da8b
C?dric Krier <ced@b2ck.com>
2021-02-12 11:04:45 +0100

    Stop building 5.4 series

--- update.sh
+++ update.sh
@@ -5,20 +5,15 @@ docker build -q --rm --no-cache -t tryton/tryton:5.8 -t tryton/tryton:latest 5.8
 docker build -q --rm --no-cache -t tryton/tryton:5.8-office -t tryton/tryton:office 5.8/office
 docker build -q --rm --no-cache -t tryton/tryton:5.6  5.6
 docker build -q --rm --no-cache -t tryton/tryton:5.6-office 5.6/office
-docker build -q --rm --no-cache -t tryton/tryton:5.4  5.4
-docker build -q --rm --no-cache -t tryton/tryton:5.4-office 5.4/office
 docker build -q --rm --no-cache -t tryton/tryton:5.0 5.0
 docker build -q --rm --no-cache -t tryton/tryton:5.0-office 5.0/office
 
 docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.8 python3 -m trytond.tests.run-tests -m
 docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.6 python3 -m trytond.tests.run-tests -m
-docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.4 python3 -m trytond.tests.run-tests -m
 docker run --rm --env DB_CACHE=/tmp --env TRYTOND_DATABASE_URI=sqlite:// tryton/tryton:5.0 python3 -m trytond.tests.run-tests -m
 
 docker push tryton/tryton:5.0-office
 docker push tryton/tryton:5.0
-docker push tryton/tryton:5.4-office
-docker push tryton/tryton:5.4
 docker push tryton/tryton:5.6-office
 docker push tryton/tryton:5.6
 docker push tryton/tryton:5.8-office
